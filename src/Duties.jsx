3f0fecc16eb93da29ca1b0574442a6cba0d084e2
Felipe <felwal1987@gmail.com>
2025-06-13 14:56:58 -0600

    added loop logic inside Duties component

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,3 +1,15 @@
+import { FaAnglesRight } from "react-icons/fa6";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Duties({ duties }) {
-  return <div>Duties</div>;
+  return (
+    <div>
+      {duties.map((duty) => (
+        <li key={duty} className="job-desc">
+          <FaAnglesRight className="job-icon" />
+          <p>{duty}</p>
+        </li>
+      ))}
+    </div>
+  );
 }
